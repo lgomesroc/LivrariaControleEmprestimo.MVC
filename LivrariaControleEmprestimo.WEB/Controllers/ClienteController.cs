817ffafe0dea5f650c30ebe6976756ab4df5983d
Luciano Gomes Rocha <]'lgomesroc@hotmail.com>
2023-11-28 13:43:44 -0300

    LivrariaControleEmprestimo MVC em construção

---
+++ LivrariaControleEmprestimo.WEB/Controllers/ClienteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LivrariaControleEmprestimo.WEB.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
